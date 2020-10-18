037b47224f3496ffbd9519000c38af7137de5880
Jared Haertel <jared.haertel@gmail.com>
2020-10-17 23:08:12 -0600

    Added RobotMap for constants and OI for user input

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotMap.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class RobotMap {
+    public static String frontLeft = "frontLeft";
+    public static String frontRight = "frontRight";
+    public static String backLeft = "backLeft";
+    public static String backRight = "backRight";
+}
