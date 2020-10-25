4fc3b3c743f109d4ce125f4a741a593ef51633fb
Jared Haertel <jared.haertel@gmail.com>
2020-10-24 18:00:20 -0600

    Added constants for shooter and drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotMap.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-import org.opencv.core.Point3;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
 
 public class RobotMap {
@@ -10,12 +10,14 @@ public class RobotMap {
     // Everything is in inches
     public static final double ROBOT_WIDTH = 18, ROBOT_HEIGHT = 18;
     // The following is relative to the robots center
-    public static final Point3 CAMERA_POS = new Point3(0,0,0);
 
-    public static final Point3 ODOMETERY_WHEEL_LEFT_POS = new Point3(0,0,0);
-    public static final Point3 ODOMETERY_WHEEL_RIGHT_POS = new Point3(0,0,0);
-    public static final Point3 ODOMETERY_WHEEL_CENTER_POS = new Point3(0,0,0);
 
+    public static final Vector3D ODOMETERY_WHEEL_LEFT_POS = new Vector3D(0,0,0);
+    public static final Vector3D ODOMETERY_WHEEL_CENTER_POS = new Vector3D(0,0,0);
+    public static final Vector3D ODOMETERY_WHEEL_RIGHT_POS = new Vector3D(0,0,0);
+
+
+    public static final double TIMEOUT = 5000; // In milliseconds
 
     // OI CONSTANTS //
     public static final double DEBOUNCER_PERIOD = 0.5;
@@ -28,6 +30,7 @@ public class RobotMap {
     public static final String CLAW_SERVO_NAME = "claw";
     public static final String ARM_SERVO_NAME = "arm";
 
+    // Position from 0 to 1
     public static final double CLAW_OPEN_POSITION = 1;
     public static final double CLAW_CLOSE_POSITION = 0;
 
@@ -37,19 +40,23 @@ public class RobotMap {
 
 
 
-
     // DRIVE CONSTANTS //
     public static final String FRONT_LEFT_NAME = "frontLeft";
     public static final String FRONT_RIGHT_NAME = "frontRight";
     public static final String BACK_LEFT_NAME = "backLeft";
     public static final String BACK_RIGHT_NAME = "backRight";
 
+    public static final double DRIVER_TARGET_ANGLE_MIN_ERROR = 1; // In degrees
+
 
 
     // INTAKE CONSTANTS //
-    public static final String INTAKE_MOTOR_NAME = "intake";
+    public static final String INTAKE_MOTOR1_NAME = "intakeFront";
+    public static final String INTAKE_MOTOR2_NAME = "intakeBack";
 
-    public static final double INTAKE_POWER_ON = 1;
+    public static final double INTAKE_MOTOR1_POWER_ON = 1;
+    public static final double INTAKE_MOTOR2_POWER_ON = 1;
+
 
 
     // POSITION LOCALIZER CONSTANTS //
@@ -61,12 +68,24 @@ public class RobotMap {
 
 
     // Shooter CONSTANTS //
-    public static final String MOTOR1_NAME = "motor1", MOTOR2_NAME = "motor2";
-    public static final double FLAP_MIN_ERROR = 0.01;
-    public static final double FLAP_MIN_ANGLE = 0; // In radians
+    public static final Vector3D SHOOTER_LOCATION = new Vector3D(0,0,0); // Relative to robot center
+
+    public static final String MOTOR1_NAME = "motor1", MOTOR2_NAME = "motor2", LOAD_MOTOR_NAME = "loadMotor";
+    public static final String FLAP_NAME = "flap";
+
+    // In radians
+    public static final double FLAP_MIN_ERROR = .03;
+    public static final double FLAP_MIN_ANGLE = 0;
     public static final double FLAP_MAX_ANGLE = 0;
+
+    public static final double SHOOTER_MIN_ERROR = 0.1;
+    public static final double SHOOTER_DEFAULT_POWER = .75;
+
+    public static final double LOAD_MOTOR_POWER = 1;
+    public static final double LOAD_MOTOR_DELAY = .5;
 
 
 
     // VISION CONSTANTS //
+    public static final Vector3D CAMERA_LOCATION = new Vector3D(0,0,0);
 }
