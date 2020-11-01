7c840b5ab7ac8e38dedb100606d9584a2ee2b993
Jared Haertel <jared.haertel@gmail.com>
2020-11-01 13:15:02 -0700

    Removed OpMode variable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/util/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/util/RobotBase.java
@@ -6,18 +6,16 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public abstract class RobotBase {
-    private OpMode opMode;
     public HardwareMap hardwareMap;
     public Telemetry telemetry;
 
     public SubsystemManager subsystemManager = new SubsystemManager();
 
     public void init(OpMode opMode) {
-        this.opMode = opMode;
         this.hardwareMap = opMode.hardwareMap;
         this.telemetry = opMode.telemetry;
     }
     // public abstract void teleopUpdate(); // Only for teleop
     public abstract void update();
-    public abstract void end();
+    public abstract void stop();
 }
