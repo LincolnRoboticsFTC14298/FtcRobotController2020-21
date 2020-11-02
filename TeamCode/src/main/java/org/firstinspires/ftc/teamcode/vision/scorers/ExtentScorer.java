05c92469cad6d149a936ff7b293a6360a43629a7
Jared Haertel <jared.haertel@gmail.com>
2020-11-02 13:46:51 -0700

    Renamed solidity to extent to fit definition

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/scorers/ExtentScorer.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.vision.scorers;
+
+import com.acmerobotics.dashboard.config.Config;
+
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+
+@Config
+public class ExtentScorer extends VisionScorer {
+    public static double optimalRatio = .8;
+    public static double weight = 1;
+
+    public ExtentScorer() {
+
+    }
+    public ExtentScorer(double optimalRatio, double weight) {
+        updateVals(optimalRatio, weight);
+    }
+
+    @Override
+    public void updateVals(double optimalRatio, double weight) {
+        this.optimalRatio = optimalRatio;
+        this.weight = weight;
+    }
+
+    @Override
+    public double score(MatOfPoint contour) {
+        Rect rect = Imgproc.boundingRect(contour);
+        double ratio = Imgproc.contourArea(contour) / rect.area();
+        return squareError(ratio, optimalRatio) * weight;
+    }
+}
