c8ade5bd0ea52878239142bab2e271b7c193a7f1
Jared Haertel <jared.haertel@gmail.com>
2020-11-01 17:27:12 -0700

    Made constants local and made them configurable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Arm.java
@@ -1,41 +1,90 @@
 package org.firstinspires.ftc.teamcode.hardware.subsystems;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.google.common.flogger.FluentLogger;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.hardware.Robot;
 import org.firstinspires.ftc.teamcode.hardware.util.Subsystem;
-import org.firstinspires.ftc.teamcode.hardware.RobotMap;
 
+@Config
 public class Arm implements Subsystem {
-    private Robot robot;
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    TelemetryPacket packet;
+
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+    public static final String CLAW_SERVO_NAME = "claw";
+    public static final String ARM_SERVO_NAME = "arm";
+
+    // Position from 0 to 1
+    public static double CLAW_OPEN_POSITION = 1;
+    public static double CLAW_CLOSE_POSITION = 0;
+
+    public static double ARM_LIFT_POSITION = 1;
+    public static double ARM_DEFAULT_POSITION = .5;
+    public static double ARM_LOWER_POSITION = 0;
 
     private Servo armServo , clawServo;
     private double armPosition, clawPosition;
 
-    public Arm(Robot robot) {
-        this.robot = robot;
+    public Arm() {
+
     }
 
     @Override
-    public void init() {
-        clawServo = robot.hardwareMap.get(Servo.class, RobotMap.CLAW_SERVO_NAME);
-        armServo = robot.hardwareMap.get(Servo.class, RobotMap.ARM_SERVO_NAME);
+    public void init(HardwareMap hardwareMap) {
+        packet = new TelemetryPacket();
+
+        clawServo = hardwareMap.get(Servo.class, CLAW_SERVO_NAME);
+        armServo = hardwareMap.get(Servo.class, ARM_SERVO_NAME);
 
         closeClaw(); // At the beginning of the round, the claw is closed with the wobble
         defaultArm();
         updateServoPositions();
+
+        dashboard.sendTelemetryPacket(packet);
     }
 
     @Override
     public void update() {
+        packet = new TelemetryPacket();
+
         updateServoPositions();
+
+        dashboard.sendTelemetryPacket(packet);
     }
 
     @Override
-    public void end() {
+    public void stop() {
+        packet = new TelemetryPacket();
+
         closeClaw();
         defaultArm();
         updateServoPositions();
+
+        dashboard.sendTelemetryPacket(packet);
+    }
+
+
+
+    public void openClaw() {
+        setClawServo(CLAW_OPEN_POSITION);
+    }
+    public void closeClaw() {
+        setClawServo(CLAW_CLOSE_POSITION);
+    }
+
+    public void liftArm() {
+        setArmAngle(ARM_LIFT_POSITION);
+    }
+    public void defaultArm() {
+        setArmAngle(ARM_DEFAULT_POSITION);
+    }
+    public void lowerArm() {
+        setArmAngle(ARM_LOWER_POSITION);
     }
 
     public void setArmAngle(double position) {
@@ -46,27 +95,11 @@ public class Arm implements Subsystem {
         this.clawPosition = position;
     }
 
-    public void openClaw() {
-        setClawServo(RobotMap.CLAW_OPEN_POSITION);
-    }
-    public void closeClaw() {
-        setClawServo(RobotMap.CLAW_CLOSE_POSITION);
-    }
-
-    public void liftArm() {
-        setArmAngle(RobotMap.ARM_LIFT_POSITION);
-    }
-    public void defaultArm() {
-        setArmAngle(RobotMap.ARM_DEFAULT_POSITION);
-    }
-    public void lowerArm() {
-        setArmAngle(RobotMap.ARM_LOWER_POSITION);
-    }
-
     public void updateServoPositions() {
         armServo.setPosition(armPosition);
         clawServo.setPosition(clawPosition);
-        robot.telemetry.addData("Arm servo positions: ", "%.5f arm, %.5f claw",
-                armServo.getPosition(), clawServo.getPosition());
+
+        packet.put("Arm position: ", armServo.getPosition());
+        packet.put("Claw position: ", clawServo.getPosition());
     }
 }
