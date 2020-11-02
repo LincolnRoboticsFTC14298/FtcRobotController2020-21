d73dd452d8e4f71dd99e65484931301b675b6eed
Jared Haertel <jared.haertel@gmail.com>
2020-11-01 17:17:11 -0700

    Added ButtonTrigger for gamepad triggers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/util/gamepad/ButtonTrigger.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.hardware.util.gamepad;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import java.lang.reflect.Field;
+
+public class ButtonTrigger extends Button {
+    private Gamepad gamepad;
+    private double threshold = 0.5;
+
+    public ButtonTrigger(Gamepad gamepad, ButtonType buttonType) {
+        super(gamepad, buttonType);
+        this.gamepad = gamepad;
+    }
+    public ButtonTrigger(Gamepad gamepad, ButtonType buttonType, double threshold) {
+        super(gamepad, buttonType);
+        this.gamepad = gamepad;
+        this.threshold = threshold;
+    }
+
+    public boolean getRawPressed() {
+        try {
+            Field f = Gamepad.class.getField(getButtonType().toString());
+            double val = (double) f.get(gamepad);
+            if (val > threshold) {
+                return true;
+            }
+        } catch (NoSuchFieldException e) {
+            //throw new IllegalStateException("Bad field name: " + button.toString(), e);
+        } catch (IllegalAccessException e) {
+            //throw new IllegalStateException("Failed to access field " + button.toString() + " after making it accessible", e);
+        }
+        return false;
+    }
+}
