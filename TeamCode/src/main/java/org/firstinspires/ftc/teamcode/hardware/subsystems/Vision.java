246df4cf086e425e400584cda9b64bae207816b1
Jared Haertel <jared.haertel@gmail.com>
2020-11-01 17:25:31 -0700

    Implemented new pipeline and made openCameraDevice async

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Vision.java
@@ -1,64 +1,109 @@
 package org.firstinspires.ftc.teamcode.hardware.subsystems;
 
-import org.firstinspires.ftc.teamcode.hardware.Robot;
+import android.graphics.Bitmap;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.google.common.flogger.FluentLogger;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.teamcode.hardware.util.Subsystem;
 import org.firstinspires.ftc.teamcode.vision.RingCountPipeline;
-
+import org.firstinspires.ftc.teamcode.vision.RingCountPipeline.Viewport;
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera2;
 
+import java.util.Arrays;
+
+import static org.opencv.android.Utils.matToBitmap;
+
 public class Vision implements Subsystem {
-    private Robot robot;
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    TelemetryPacket packet;
 
-    OpenCvInternalCamera2 phoneCam;
-    RingCountPipeline ringCountPipeline;
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
-    private int rings = -1;
+    private OpenCvInternalCamera2 phoneCam;
+    private RingCountPipeline ringCountPipeline;
 
-    // Can delete later
+    private int rings;
+
+    // TODO: Possibly delete later
     private boolean processed = false;
 
-    public Vision(Robot robot) {
-        this.robot = robot;
+    public Vision() {
+
     }
 
     @Override
-    public void init() {
-        this.robot = robot;
-        int cameraMonitorViewId = robot.hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", robot.hardwareMap.appContext.getPackageName());
-        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK, cameraMonitorViewId);
-        phoneCam.openCameraDevice();
-        ringCountPipeline = new RingCountPipeline(phoneCam,true);
-        phoneCam.setPipeline(ringCountPipeline);
+    public void init(HardwareMap hardwareMap) {
+        packet = new TelemetryPacket();
 
-        phoneCam.setSensorFps(30);
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK, cameraMonitorViewId);
+        phoneCam.openCameraDeviceAsync(
+                new OpenCvCamera.AsyncCameraOpenListener() {
+                    @Override
+                    public void onOpened() {
+                        ringCountPipeline = new RingCountPipeline(phoneCam, Viewport.ANNOTATED);
+                        phoneCam.setPipeline(ringCountPipeline);
+                        phoneCam.setSensorFps(30);
+                    }
+                }
+        );
+
+        dashboard.sendTelemetryPacket(packet);
     }
 
     @Override
     public void update() {
-
+        packet = new TelemetryPacket();
+        dashboard.sendTelemetryPacket(packet);
     }
 
     @Override
-    public void end() {
-
+    public void stop() {
+        packet = new TelemetryPacket();
+        stopStreaming();
+        dashboard.sendTelemetryPacket(packet);
     }
 
     public void analyze() {
-        phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
         rings = ringCountPipeline.getNumRingsFound();
-        phoneCam.stopStreaming();
         processed = true;
-        robot.telemetry.addData("Number of Rings: ", rings);
+        packet.put("Number of Rings: ", rings);
+    }
+
+    public void startStreaming() {
+        phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+    }
+    public void stopStreaming() {
+        phoneCam.stopStreaming();
+    }
+
+    public void setViewport(Viewport viewport) {
+        ringCountPipeline.setViewport(viewport);
+    }
+
+    public Bitmap getOutput() {
+        Mat mat = ringCountPipeline.getLatestMat();
+        Bitmap bmt = null;
+        matToBitmap(mat, bmt);
+        return bmt;
     }
 
     public int getNumRings() {
-        if (processed) {
+        if (processed && Arrays.asList(new int[]{0,1,4}).contains(rings)) {
             return rings;
+        } else if (processed) {
+            packet.addLine("Ring is not 0,1,4!");
+            return 0;
         } else {
-            robot.telemetry.addData("ERROR: ", "IMAGE NOT PROCESSED");
-            return -1;
+            packet.addLine("ERROR: IMAGE NOT PROCESSED");
+            return 0;
         }
     }
 }
