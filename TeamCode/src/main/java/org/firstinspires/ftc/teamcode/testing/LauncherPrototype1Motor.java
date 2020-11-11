6d015ed2bc2cd55a0861f8173d238ded05a5a10e
Jared Haertel <jared.haertel@gmail.com>
2020-11-11 09:59:20 -0700

    Added launcher prototype 1 motor which only has one motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/LauncherPrototype1Motor.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name="Launcher 2 Motors", group="Prototype")
+public class LauncherPrototype1Motor extends LinearOpMode {
+    public final double MOTOR_STEP = 0.05; // Motor speed step
+
+    @Override
+    public void runOpMode() {
+        double mSpeed = 0;
+
+        DcMotor motor1 = hardwareMap.get(DcMotor.class, "motor1");
+
+        // Uncomment one of these if you want to reverse one of the motors
+        // May do this for the motors on opposite side
+        // Forward may be clockwise but I forgot
+
+        //motor1.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motor1.setPower(mSpeed);
+
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+            // Up and down updates the first motor speed
+            // Left and right updates the second motor speed
+            if (gamepad1.dpad_up) {
+                mSpeed += MOTOR_STEP;
+            } if (gamepad1.dpad_down) {
+                mSpeed -= MOTOR_STEP;
+            }
+
+            motor1.setPower(mSpeed);
+
+            // Telemetry
+            telemetry.addData("Motor power: ", mSpeed);
+        }
+    }
+}
