72377e23a0be5d4dc7bb336de874d04e834a219f
Jared Haertel <jared.haertel@gmail.com>
2020-12-14 23:07:39 -0700

    Added calculations for target launch angle to PositionProvider

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Shooter.java
@@ -8,9 +8,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
-import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
-
 import robotlib.hardware.Subsystem;
+import robotlib.util.MathUtil;
 
 @Config
 public class Shooter implements Subsystem {
@@ -18,7 +17,7 @@ public class Shooter implements Subsystem {
     //private static final FluentLogger logger = FluentLogger.forEnclosingClass();
     //private static final Telemetry telemetry = new Telemetry("Shooter");
 
-    private static final String MOTOR1_NAME = "motor1", MOTOR2_NAME = "motor2";
+    private static final String SHOOTER_MOTOR1_NAME = "shooter1", SHOOTER_MOTOR2_NAME = "shooter2";
     private static final String FLAP_NAME = "flap", LAUNCH_FLAP_NAME = "launchFlap";
 
     public static double SHOOTER_MIN_ERROR = 0.1;
@@ -35,32 +34,25 @@ public class Shooter implements Subsystem {
     public static double LAUNCH_FLAP_EXTENDED_POS = 1;
 
 
-    private static final double LAMBDA = 1; // = g / (2*v0*v0) optimal lambda will be found and used
+    private DcMotor shooterMotor1, shooterMotor2;
+    private Servo flapServo, launchFlapServo;
 
-    private DcMotor motor1, motor2;
-    private Servo flap, launchFlap;
-
-    private double motor1Power, motor2Power;
+    private double shooterMotor1Power, shooterMotor2Power;
     private double flapAngle, launchFlapPos;
     private double targetAngle = 0;
 
-//    private Target target = Target.HIGH_GOAL;
-//    private Alliance alliance = Alliance.BLUE;
-
     private PositionProvider positionProvider;
-
-    private double shootScheduler = 0;
-    private boolean loading = false;
 
     public Shooter(HardwareMap hardwareMap, PositionProvider positionProvider) {
         // Initialize motors and servos //
-        motor1 = hardwareMap.get(DcMotor.class, MOTOR1_NAME);
-        motor2 = hardwareMap.get(DcMotor.class, MOTOR2_NAME);
+        shooterMotor1 = hardwareMap.get(DcMotor.class, SHOOTER_MOTOR1_NAME);
+        shooterMotor2 = hardwareMap.get(DcMotor.class, SHOOTER_MOTOR2_NAME);
 
-        flap = hardwareMap.get(Servo.class, FLAP_NAME);
-        launchFlap = hardwareMap.get(Servo.class, LAUNCH_FLAP_NAME);
+        flapServo = hardwareMap.get(Servo.class, FLAP_NAME);
+        launchFlapServo = hardwareMap.get(Servo.class, LAUNCH_FLAP_NAME);
 
         this.positionProvider = positionProvider;
+
         // Reverse direction
 //        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 //        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -83,43 +75,50 @@ public class Shooter implements Subsystem {
         retractLaunchFlap();
     }
 
+    @Override
+    public void updateMotorsAndServos() {
+        shooterMotor1.setPower(shooterMotor1Power);
+        shooterMotor2.setPower(shooterMotor2Power);
+
+        flapServo.setPosition(angleToPos(flapAngle));
+        launchFlapServo.setPosition(launchFlapPos);
+
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.put("Shooter power: ", shooterMotor1.getPower());
+        packet.addLine("Flap Angle: " +  Math.toDegrees(targetAngle) + " target vs " +
+                Math.toDegrees(posToAngle(flapServo.getPosition())) + " actual");
+        dashboard.sendTelemetryPacket(packet);
+    }
+
 
     public boolean doneAiming() {
-        return Math.abs(posToAngle(flap.getPosition()) - flapAngle) < FLAP_MIN_ERROR;
+        return Math.abs(posToAngle(flapServo.getPosition()) - flapAngle) < FLAP_MIN_ERROR;
     }
     public void aimAsync() {
-        Vector3D targetRelativePos = positionProvider.getTargetRelativeLocation();
-        double dist = Math.hypot(targetRelativePos.getY(), targetRelativePos.getX());
-        double height = targetRelativePos.getZ();
-
-        // Math stuff
-        double k = 1.0/(2*dist*LAMBDA);
-        double det = 1 - 4*(height + LAMBDA*dist*dist)*LAMBDA;
-        targetAngle = Math.atan(k - k * Math.sqrt(det));
-
-        setFlapAngle(targetAngle);
-    }
-    public void aim() {
-        aimAsync();
-        while (!doneAiming()) {
-            update();
-            updateMotorsAndServos();
-        }
+        setFlapAngle(positionProvider.getTargetLaunchAngle());
     }
 
     public boolean readyToLaunch() {
         return doneAiming() &&
-                Math.abs(motor1.getPower() - motor1Power) < SHOOTER_MIN_ERROR &&
-                Math.abs(motor2.getPower() - motor2Power) < SHOOTER_MIN_ERROR;
+                MathUtil.withinRange(positionProvider.getTargetLaunchAngle(), FLAP_MIN_ANGLE, FLAP_MAX_ANGLE) &&
+                Math.abs(shooterMotor1.getPower() - shooterMotor1Power) < SHOOTER_MIN_ERROR &&
+                Math.abs(shooterMotor2.getPower() - shooterMotor2Power) < SHOOTER_MIN_ERROR;
+    }
+
+    public boolean isExtended() {
+        return Math.abs(launchFlapServo.getPosition() - LAUNCH_FLAP_EXTENDED_POS) < LAUNCH_FLAP_EXTENDED_MIN_ERROR;
+    }
+    public boolean isRetracted() {
+        return Math.abs(launchFlapServo.getPosition() - LAUNCH_FLAP_RETRACTED_POS) < LAUNCH_FLAP_RETRACTED_MIN_ERROR;
     }
 
     public void launch() {
         extendLaunchFlap();
-        while (Math.abs(launchFlap.getPosition() - launchFlapPos) < LAUNCH_FLAP_EXTENDED_MIN_ERROR) {
+        while (!isExtended()) {
             updateMotorsAndServos();
         }
         retractLaunchFlap();
-        while (Math.abs(launchFlap.getPosition() - launchFlapPos) < LAUNCH_FLAP_RETRACTED_MIN_ERROR) {
+        while (!isRetracted()) {
             updateMotorsAndServos();
         }
     }
@@ -132,14 +131,12 @@ public class Shooter implements Subsystem {
         setShooterPower(0);
     }
 
-
-
     // Angle is in radians
     public double getTargetAngle() {
         return flapAngle;
     }
     public void setFlapAngle(double angle) {
-        flapAngle = angle;
+        flapAngle = Range.clip(angle, FLAP_MIN_ANGLE, FLAP_MAX_ANGLE);
     }
 
     public void retractLaunchFlap() {
@@ -150,9 +147,8 @@ public class Shooter implements Subsystem {
     }
 
     private void setShooterPower(double power) {
-        // TODO: Clip powers to make sure it's between -1 and 1
-        double p = Range.clip(power, -1, 1);
-        motor1Power = motor2Power = p;
+        power = Range.clip(power, -1, 1);
+        shooterMotor1Power = shooterMotor2Power = power;
     }
 
     private double posToAngle(double pos) {
@@ -162,20 +158,5 @@ public class Shooter implements Subsystem {
     private double angleToPos(double angle) {
         // Scale from minAngle = 0 to maxAngle = 1
         return Range.scale(angle, FLAP_MIN_ANGLE, FLAP_MAX_ANGLE, 0, 1);
-    }
-
-    @Override
-    public void updateMotorsAndServos() {
-        motor1.setPower(motor1Power);
-        motor2.setPower(motor2Power);
-
-        flap.setPosition(angleToPos(flapAngle));
-        launchFlap.setPosition(launchFlapPos);
-
-        TelemetryPacket packet = new TelemetryPacket();
-        packet.put("Shooter power: ", motor1.getPower());
-        packet.addLine("Flap Angle: " +  Math.toDegrees(targetAngle) + " target vs " +
-                Math.toDegrees(posToAngle(flap.getPosition())) + " actual");
-        dashboard.sendTelemetryPacket(packet);
     }
 }
