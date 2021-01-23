a54d6dff156c667abd0f12d5347a4f00a9070fbf
Jared Haertel <jared.haertel@gmail.com>
2021-01-23 15:17:05 -0700

    Revert "added adb tools"
    This reverts commit 789acbf4

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleOp.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.opmodes.DataWriterUtil;
+
+@TeleOp(name="Main TeleOp", group="TeleOp")
+@Disabled
+public class MainTeleOp extends OpMode {
+    Robot robot;
+    OperatorInterface operatorInterface = new OperatorInterface(robot, gamepad1, gamepad2);
+
+    @Override
+    public void init() {
+        robot = new Robot(this);
+
+        robot.setAlliance(DataWriterUtil.readAlliance());
+        robot.setPoseEstimate(DataWriterUtil.readLastPose());
+
+        robot.init();
+    }
+
+    @Override
+    public void start() {
+        robot.start();
+    }
+
+    @Override
+    public void loop() {
+        operatorInterface.update();
+        robot.update();
+    }
+
+    @Override
+    public void stop() {
+        robot.stop();
+    }
+}
