1f0d83f369fc5f0ec140fab250fa8e79129a91de
Jared Haertel <jared.haertel@gmail.com>
2021-02-07 00:28:19 -0700

    Added tunable lowerThreshold and upperThreshold

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/operators/HSVRangeFilter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/operators/HSVRangeFilter.java
@@ -11,27 +11,28 @@ import org.opencv.imgproc.Imgproc;
 
 @Config
 public class HSVRangeFilter implements MatOperator {
-    public static Scalar lowerThreshold = new Scalar(10, 100, 120);
-    public static Scalar upperThreshold = new Scalar(20, 255, 255);
+    public static int lowerThresholdR = 10;
+    public static int lowerThresholdG = 100;
+    public static int lowerThresholdB = 120;
+    public static int upperThresholdR = 20;
+    public static int upperThresholdG = 255;
+    public static int upperThresholdB = 255;
     public static Size blur = new Size(5,5);
     public static int blurSigma = 1;
 
     public HSVRangeFilter() {
 
     }
-    public HSVRangeFilter(Scalar lowerThreshold, Scalar upperThreshold) {
-        HSVRangeFilter.lowerThreshold = lowerThreshold;
-        HSVRangeFilter.upperThreshold = upperThreshold;
-    }
-    public HSVRangeFilter(Scalar lowerThreshold, Scalar upperThreshold, Size blur) {
-        HSVRangeFilter.lowerThreshold = lowerThreshold;
-        HSVRangeFilter.upperThreshold = upperThreshold;
+    public HSVRangeFilter(Size blur) {
         HSVRangeFilter.blur = blur;
     }
 
     Mat mask = new Mat();
     @Override
     public Mat process(Mat img) {
+        Scalar lowerThreshold = new Scalar(lowerThresholdR, lowerThresholdG, lowerThresholdB);
+        Scalar upperThreshold = new Scalar(upperThresholdR, upperThresholdG, upperThresholdB);
+
         Imgproc.cvtColor(img, mask, Imgproc.COLOR_RGB2HSV);
 
         Imgproc.GaussianBlur(mask, mask, blur, blurSigma);
