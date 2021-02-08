9d671df303562d5fdc562b6790f07c076cf0a906
Jared Haertel <jared.haertel@gmail.com>
2021-02-07 18:22:57 -0700

    Changed button class

--- TeamCode/src/main/java/org/firstinspires/ftc/robotlib/hardware/gamepad/ButtonTrigger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/robotlib/hardware/gamepad/ButtonTrigger.java
@@ -7,26 +7,34 @@ import java.lang.reflect.Field;
 public class ButtonTrigger extends Button {
     private Gamepad gamepad;
     private double threshold = 0.5;
+    private Field field;
 
-    public ButtonTrigger(Gamepad gamepad, org.firstinspires.ftc.robotlib.hardware.gamepad.ButtonType buttonType) {
+    public ButtonTrigger(Gamepad gamepad, ButtonType buttonType) {
         super(gamepad, buttonType);
         this.gamepad = gamepad;
+        try {
+            this.field = Gamepad.class.getField(buttonType.toString());
+        } catch (Exception e) {
+            this.field = null;
+        }
     }
     public ButtonTrigger(Gamepad gamepad, ButtonType buttonType, double threshold) {
         super(gamepad, buttonType);
         this.gamepad = gamepad;
         this.threshold = threshold;
+        try {
+            this.field = Gamepad.class.getField(buttonType.toString());
+        } catch (Exception e) {
+            this.field = null;
+        }
     }
 
     public boolean getRawPressed() {
         try {
-            Field f = Gamepad.class.getField(getButtonType().toString());
-            double val = (double) f.get(gamepad);
+            double val = (double) field.get(gamepad);
             if (val > threshold) {
                 return true;
             }
-        } catch (NoSuchFieldException e) {
-            //throw new IllegalStateException("Bad field name: " + button.toString(), e);
         } catch (IllegalAccessException e) {
             //throw new IllegalStateException("Failed to access field " + button.toString() + " after making it accessible", e);
         }
