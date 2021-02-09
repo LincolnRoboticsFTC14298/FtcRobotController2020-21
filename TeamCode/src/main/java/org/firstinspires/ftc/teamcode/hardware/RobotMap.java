b3c48385d4f4e68a89a985e5782d409c3e00b722
Jared Haertel <jared.haertel@gmail.com>
2021-02-09 15:15:26 -0700

    Added Vector2d of robot parts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotMap.java
@@ -1,18 +1,20 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
-import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
-
-@Config
 
 public class RobotMap {
     // ROBOT INFO //
     // Everything is in inches //
-    public static double TIMEOUT = 5000; // In milliseconds
 
     // The following is relative to the robots center
-    public static Vector3D SHOOTER_LOCATION = new Vector3D(0,0,0); // Relative to robot center
+    public static Vector3D SHOOTER_LOCATION = new Vector3D(0,0,0);
+    public static Vector2d SHOOTER_LOCATION_2d = new Vector2d(SHOOTER_LOCATION.getX(), SHOOTER_LOCATION.getY());
+
     public static Vector3D CAMERA_LOCATION = new Vector3D(0,0,0);
+    public static Vector2d CAMERA_LOCATION_2d = new Vector2d(CAMERA_LOCATION.getX(), CAMERA_LOCATION.getY());
+
     public static Vector3D ARM_DOWN_LOCATION = new Vector3D(0,0,0);
+    public static Vector2d ARM_DOWN_LOCATION_2d = new Vector2d(ARM_DOWN_LOCATION.getX(), ARM_DOWN_LOCATION.getY());
 }
