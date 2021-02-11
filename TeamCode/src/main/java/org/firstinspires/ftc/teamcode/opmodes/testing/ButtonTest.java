32fe1c0a4a40815b58ee51be39167f455e11ee04
Jared Haertel <jared.haertel@gmail.com>
2021-02-10 22:42:32 -0700

    Added edge cases

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/ButtonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/ButtonTest.java
@@ -1,34 +1,40 @@
 package org.firstinspires.ftc.teamcode.opmodes.testing;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotlib.hardware.gamepad.Button;
+import org.firstinspires.ftc.robotlib.hardware.gamepad.ButtonTrigger;
 import org.firstinspires.ftc.robotlib.hardware.gamepad.ButtonType;
 import org.firstinspires.ftc.robotlib.hardware.gamepad.ToggleButton;
 
 @TeleOp(name="Button test", group="Test")
-@Disabled
 public class ButtonTest extends OpMode {
     Button b;
     ToggleButton x, y;
-    int bt = 0;
+    ButtonTrigger r, l;
+    int bt = 0, xt = 0, rt = 0, st = 0;
 
 
     @Override
     public void init() {
         b = new Button(gamepad1, ButtonType.b);
-        x = new ToggleButton(gamepad1, ButtonType.x);
-        y = new ToggleButton(gamepad1, ButtonType.y, 5);
+        x = new ToggleButton(gamepad1, ButtonType.x, 1);
+        r = new ButtonTrigger(gamepad1, ButtonType.right_trigger);
+        l = new ButtonTrigger(gamepad1, ButtonType.left_trigger);
     }
 
     @Override
     public void loop() {
         if (b.isPressed()) bt++;
+        if (x.changedToOn()) xt++;
+        if (r.isPressed()) rt++;
+        if (r.isPressed() && l.isPressed()) st++;
         telemetry.addData("Times b has been pressed: ", bt);
         telemetry.addData("Toggle x = ", x.on());
-        telemetry.addData("Debounce toggle y w/ 5 sec debounce = ", y.on());
+        telemetry.addData("Times x changed from off to on: ", xt);
+        telemetry.addData("Times right trigger button pressed: ", rt);
+        telemetry.addData("Times right and left trigger buttons pressed together: ", st);
         telemetry.update();
     }
 }
