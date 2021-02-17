736e7cf5988321e9ec221d1fb187a2e8addb52e6
Jared Haertel <jared.haertel@gmail.com>
2021-02-16 17:42:32 -0700

    Made RobotBase an interface

--- TeamCode/src/main/java/org/firstinspires/ftc/robotlib/hardware/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/robotlib/hardware/RobotBase.java
@@ -1,24 +1,9 @@
 package org.firstinspires.ftc.robotlib.hardware;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-public abstract class RobotBase {
-    public HardwareMap hardwareMap;
-    public Telemetry telemetry;
-
-    public SubsystemManager subsystemManager = new SubsystemManager();
-
-    public RobotBase(OpMode opMode) {
-        this.hardwareMap = opMode.hardwareMap;
-        this.telemetry = opMode.telemetry;
-    }
-
-    public abstract void init();
-    public abstract void initUpdate();
-    public abstract void start();
-    public abstract void update();
-    public abstract void stop();
+public interface RobotBase {
+    void init();
+    void initUpdate();
+    void start();
+    void update();
+    void stop();
 }
