185fba1452c18ba4c7b01e0b0966421fded2c178
Jared Haertel <jared.haertel@gmail.com>
2021-02-16 17:45:54 -0700

    Robot takes hardwareMap and telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/ShooterTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/ShooterTuner.java
@@ -19,7 +19,7 @@ public class ShooterTuner extends OpMode {
 
     @Override
     public void init() {
-        robot = new Robot(this);
+        robot = new Robot(hardwareMap, telemetry);
         gamepad = new RadicalGamepad(gamepad1);
     }
 
