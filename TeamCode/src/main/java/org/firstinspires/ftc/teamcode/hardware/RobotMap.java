89348f6e0f7020160b15a58df4428f1d88d2a560
Jared Haertel <jared.haertel@gmail.com>
2021-02-16 23:01:47 -0700

    Used Vector3D to Vector2d

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotMap.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.hardware;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.firstinspires.ftc.robotlib.util.MathUtil;
 
 public class RobotMap {
     // ROBOT INFO //
@@ -10,13 +11,13 @@ public class RobotMap {
 
     // The following is relative to the robots center
     public static Vector3D SHOOTER_LOCATION = new Vector3D(0,0,0);
-    public static Vector2d SHOOTER_LOCATION_2d = new Vector2d(SHOOTER_LOCATION.getX(), SHOOTER_LOCATION.getY());
+    public static Vector2d SHOOTER_LOCATION_2d = MathUtil.vector3DToVector2d(SHOOTER_LOCATION);
 
     public static double CAMERA_PITCH = 0; // RADIANS, positive if pointing down
     public static double CAMERA_YAW = 0;
     public static Vector3D CAMERA_LOCATION = new Vector3D(0,0,0);
-    public static Vector2d CAMERA_LOCATION_2d = new Vector2d(CAMERA_LOCATION.getX(), CAMERA_LOCATION.getY());
+    public static Vector2d CAMERA_LOCATION_2d = MathUtil.vector3DToVector2d(CAMERA_LOCATION);
 
     public static Vector3D ARM_DOWN_LOCATION = new Vector3D(0,0,0);
-    public static Vector2d ARM_DOWN_LOCATION_2d = new Vector2d(ARM_DOWN_LOCATION.getX(), ARM_DOWN_LOCATION.getY());
+    public static Vector2d ARM_DOWN_LOCATION_2d = MathUtil.vector3DToVector2d(ARM_DOWN_LOCATION);
 }
