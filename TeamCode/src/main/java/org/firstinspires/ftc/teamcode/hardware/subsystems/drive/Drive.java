4fe64014463e6075976cbb6323e174977a9c256e
Jared Haertel <jared.haertel@gmail.com>
2021-02-20 13:40:21 -0700

    Doesn't need to go behind the line if it already is

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/drive/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/drive/Drive.java
@@ -338,9 +338,12 @@ public class Drive extends MecanumDrive {
     }
 
     public void goBehindLineAsync() {
-        Vector2d targetVec = new Vector2d(LAUNCH_X, localizer.getPoseEstimate().getY());
-        double angle = localizer.getTargetHeading(targetVec);
-        strafeToPointAsync(new Pose2d(targetVec, angle));
+        // Doesn't need to if already behind line
+        if (!isBehindLine()) {
+            Vector2d targetVec = new Vector2d(LAUNCH_X, localizer.getPoseEstimate().getY());
+            double angle = localizer.getTargetHeading(targetVec);
+            strafeToPointAsync(new Pose2d(targetVec, angle));
+        }
     }
     public boolean isBehindLine() {
         return getPoseEstimate().getX() < LAUNCH_X + BEHIND_LINE_ERROR;
