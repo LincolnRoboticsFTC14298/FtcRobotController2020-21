d08e9598195e9eba79ecda7adcb4859567d77952
Jared Haertel <jared.haertel@gmail.com>
2021-02-20 21:46:18 -0700

    Cleaned up code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DataWriterUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DataWriterUtil.java
@@ -11,14 +11,14 @@ public abstract class DataWriterUtil {
     public static Field.Alliance getAlliance() {
         return alliance;
     }
-    public static void setAlliance(Field.Alliance _alliance) {
-        alliance = _alliance;
+    public static void setAlliance(Field.Alliance alliance) {
+        DataWriterUtil.alliance = alliance;
     }
 
     public static Pose2d getLastPose() {
         return lastPose;
     }
-    public static void setLastPose(Pose2d _lastPose) {
-        lastPose = _lastPose;
+    public static void setLastPose(Pose2d lastPose) {
+        DataWriterUtil.lastPose = lastPose;
     }
 }
