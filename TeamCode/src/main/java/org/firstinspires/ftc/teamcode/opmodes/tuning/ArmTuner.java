66f7b69d72a51b4264a32efe834a8a8c0b82024c
Jared Haertel <jared.haertel@gmail.com>
2021-03-21 11:22:24 -0600

    Made arm tuner manually change angles and positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/ArmTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/ArmTuner.java
@@ -1,32 +1,39 @@
 package org.firstinspires.ftc.teamcode.opmodes.tuning;
 
-import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotlib.hardware.SubsystemManager;
 import org.firstinspires.ftc.robotlib.hardware.gamepad.RadicalGamepad;
 import org.firstinspires.ftc.teamcode.hardware.subsystems.Arm;
 
-@Config
 @TeleOp(name="Arm Tuner", group="Tuner")
-@Disabled
 public class ArmTuner extends OpMode {
     private SubsystemManager subsystemManager = new SubsystemManager();
     private Arm arm;
     private RadicalGamepad gamepad;
+
+    public static double incrClaw = 0.005;
+    public static double incrArm = 0.005;
+
+    private double clawPos = .5;
+    private double armAngle = 1;
 
     @Override
     public void init() {
         arm = new Arm(hardwareMap);
         subsystemManager.add(arm);
         gamepad = new RadicalGamepad(gamepad1);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
     }
 
     @Override
     public void start() {
-        subsystemManager.start();
+        arm.defaultPos();
     }
 
     @Override
@@ -34,16 +41,27 @@ public class ArmTuner extends OpMode {
         gamepad.update();
 
         if (gamepad.a) {
-            arm.openClaw();
+            clawPos += incrClaw;
         } else if (gamepad.b) {
-            arm.closeClaw();
+            clawPos -= incrClaw;
         } else if (gamepad.x) {
-            arm.lift();
+            armAngle += incrArm;
         } else if (gamepad.y) {
-            arm.defaultPos();
+            armAngle -= incrArm;
+        } else if (gamepad.left_bumper) {
+            arm.resetDefaultAngle(armAngle);
         } else if (gamepad.right_bumper) {
-            arm.lower();
+            arm.defaultPos();
         }
+
+        clawPos = Range.clip(clawPos, 0, 1);
+        armAngle = Range.clip(armAngle, 0, Math.PI * 2 / 3.0);
+
+        arm.setClawPosition(clawPos);
+        arm.setArmAngle(armAngle);
+
+        telemetry.addData("Claw Pos: ", clawPos);
+        telemetry.addData("Arm Angle: ", armAngle);
 
         subsystemManager.update();
     }
