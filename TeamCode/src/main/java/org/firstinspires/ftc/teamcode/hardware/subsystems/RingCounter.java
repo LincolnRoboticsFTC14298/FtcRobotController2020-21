4428e4a744474937e68da0389cf83da4c139be31
Jared Haertel <jared.haertel@gmail.com>
2021-04-12 11:52:38 -0600

    Added color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/RingCounter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/RingCounter.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.hardware.subsystems;
 
+import android.graphics.Color;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.google.common.flogger.FluentLogger;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotlib.hardware.AbstractSubsystem;
 import org.firstinspires.ftc.robotlib.util.MathUtil;
@@ -19,11 +23,15 @@ public class RingCounter extends AbstractSubsystem {
     private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
     private static final String DISTANCE_SENSOR_NAME = "distanceSensor";
+    private static final String COLOR_SENSOR_NAME = "colorSensor";
 
     //TODO: ADD FRONT AND CARTRIDGE SENSOR
     public static double WALL_DIST_MIN_ERROR = .2; // in
     public static double RING_DIST_MIN_ERROR = .5;
     public static double WALL_DIST = 4;
+
+    public static Vector3D RING_COLOR = new Vector3D(0,0,0); // HSV
+    public static double RING_COLOR_MIN_ERROR = 1;
 
     public static double TIME_FROM_FRONT_TO_CARTRIDGE = 1000; // ms
 
@@ -33,21 +41,36 @@ public class RingCounter extends AbstractSubsystem {
     private double currDist;
     private double dx;
 
-    private int numOfRingsCartridge;
+    private double[] hsvValues = new double[3];
+
+    private int numOfRingsCartridge; // An approximation
     private int totalRings;
 
     private DistanceSensor distanceSensor;
+    private ColorSensor colorSensor;
 
     private boolean reversed = false;
 
     public RingCounter(HardwareMap hardwareMap) {
         super("Ring Counter");
         distanceSensor = hardwareMap.get(DistanceSensor.class, DISTANCE_SENSOR_NAME);
+        colorSensor = hardwareMap.get(ColorSensor.class, COLOR_SENSOR_NAME);
+    }
+
+    @Override
+    public void init() {
+        colorSensor.enableLed(true);
     }
 
     @Override
     public void readSensorValues() {
         currDist = distanceSensor.getDistance(DistanceUnit.INCH); // read sensor
+
+        float[] hsv = new float[3];
+        Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsv);
+        for (int i = 0; i < hsvValues.length; i++) {
+            hsvValues[i] = hsv[i];
+        }
     }
 
     @Override
@@ -67,6 +90,7 @@ public class RingCounter extends AbstractSubsystem {
         telemetry.put("Total", totalRings);
         telemetry.put("Reversed", reversed);
         telemetry.put("Curr Dist", currDist);
+        telemetry.put("Color", hsvValues);
     }
 
     @Override
@@ -75,6 +99,7 @@ public class RingCounter extends AbstractSubsystem {
         logger.atInfo().log("Total: %f", totalRings);
         logger.atInfo().log("Reversed: %f", reversed);
         logger.atInfo().log("Sensor distance: %f", currDist);
+        logger.atInfo().log("Color reading: %f", hsvValues);
     }
 
     private void updateFront() {
@@ -92,6 +117,13 @@ public class RingCounter extends AbstractSubsystem {
 
     private void updateCartridge() {
         // In order from oldest to youngest
+        if (isCartridgeFull() && numOfRingsCartridge < 3) {
+            for (int i = 0; i < 3-numOfRingsCartridge; i++) {
+                rings.removeFirst();
+            }
+            numOfRingsCartridge = 3;
+        }
+
         // TODO: Would break if forward -> reverse -> forward, may not be a problem
         while (rings.getFirst().milliseconds() >= TIME_FROM_FRONT_TO_CARTRIDGE) {
             rings.removeFirst();
@@ -125,6 +157,11 @@ public class RingCounter extends AbstractSubsystem {
     }
     public boolean allRingsInCartridge() {
         return totalRings == numOfRingsCartridge;
+    }
+
+    public boolean isCartridgeFull() {
+        double d = Vector3D.distance(RING_COLOR, new Vector3D(hsvValues));
+        return MathUtil.differenceWithinError(d, 0, RING_COLOR_MIN_ERROR);
     }
 
     public void setReversed(boolean reversed) {
