d042bdd530f4f2db29e25ba99a5a730409b08da1
Jared Haertel <jared.haertel@gmail.com>
2021-04-15 10:16:26 -0700

    Goes to collecting instead of traveling to wobble goal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/statemachine/TravelingToDropOffWobbleGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/statemachine/TravelingToDropOffWobbleGoal.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop.statemachine;
 
 import org.firstinspires.ftc.robotlib.util.statemachine.AbstractState;
 import org.firstinspires.ftc.teamcode.opmodes.teleop.MainTeleOp;
-import org.firstinspires.ftc.teamcode.util.Field;
 
 public class TravelingToDropOffWobbleGoal extends AbstractNavigationState {
 
@@ -20,20 +19,16 @@ public class TravelingToDropOffWobbleGoal extends AbstractNavigationState {
         if (getTeleOp().getControlMode() == MainTeleOp.ControlMode.FULLY_MANUAL) {
             return new Manual(getTeleOp());
         } else if (!getRobot().drive.isBusy() && getRobot().drive.isAtWall()) {
-            if (Field.wobbleGoalProvider.amount() > 0) {
-                return new TravelingToWobbleGoal(getTeleOp());
-            } else {
-                getRobot().arm.closeClaw();
-                return new Collecting(getTeleOp());
-            }
+            return new Collecting(getTeleOp());
         }
         return this;
     }
 
     @Override
     public void end() {
-        getRobot().arm.openClaw();
+        // Drop wobble goal
+        getRobot().arm.middle();
         // May need to sleep or some IS_DROPPING state
-        getRobot().arm.lower();
+        getRobot().arm.openClaw();
     }
 }
